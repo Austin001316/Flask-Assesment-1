daef97d7df7da19522d79b87725a6453a370ab98
Austin001316 <91763031+Austin001316@users.noreply.github.com>
2022-08-10 15:23:01 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyCodes
+import requests
 
+rates = CurrencyRates()
+codes = CurrencyCodes()
 app = Flask(__name__)
 
 @app.route('/')
-def converterApp():
-    html = """
-    <html>
-        <body>
-            <h1>This is my currency converter.</h1>
-            <p>Please enter the currency you want to convert from and the currency you want to convert to.</p>
-                <form>
-                    <input type="text" id="currency1" placeholder="First currency"/>
-                    <input type="text" id="currency2" placeholder="Second currency"/>
-                    <input type="text" id="amout" placeholder="Amount"/>
-                </form>
-        </body>
-    </html>
-    """
-    return html
+def home():
+    return render_template('home.html')
 
-c = CurrencyRates()
+@app.route('/convert', methods=['POST'])
+def convert():
+    currency1 = request.form.get('input1')
+    currency2 = request.form.get('input2')
+    amount = request.form.get('amount')
+    unimportantvariable = codes.get_symbol(currency1)
+    Symbol = codes.get_symbol(currency2)
+    if unimportantvariable is None:
+        return render_template('home.html', error="Currency 1 is invalid, please make sure a valid currency code is entered.")
+    elif Symbol is None:
+        return render_template('home.html', error="Currency 2 is invalid, please make sure a valid currency code is entered.")
+    elif unimportantvariable != None:
+        try:
+            amount = int(amount)
+        except Exception as e:
+            return render_template('home.html', error="Amount is invalid, please make sure it is a positive whole number.")
+        if amount < 0:
+            return render_template('home.html', error="Amount is a negative, please change it to a positive whole number.")
+        elif amount is 0:
+            return render_template('home.html', error="Amount is 0, 0 will always convert to 0. Try a positive whole number.")
+        else:
+            try:
+                result = rates.convert(currency1, currency2, amount)
+                result = round(result, 2)
+                return render_template('convert.html', currency1=currency1, currency2=currency2, amount=amount, result=result, Symbol=Symbol)
 
-    c.get_rate('input1', 'input2')
-
-set FLASK_APP=app.py:app
+            except Exception as e:
+                return render_template('home.html', error="Please verify you are using valid currencies from the link below.")
